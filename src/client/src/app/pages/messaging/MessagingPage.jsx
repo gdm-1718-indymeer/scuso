01070ef6ef8da6eac724c8e205c058cb7a7be01f
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-05-26 23:09:22 +0200

    test

--- src/client/src/app/pages/messaging/MessagingPage.jsx
+++ src/client/src/app/pages/messaging/MessagingPage.jsx
@@ -2,9 +2,9 @@
 Import extenal libraries
 */
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import ReactDOM from 'react-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 /*
 Import internal libraries
