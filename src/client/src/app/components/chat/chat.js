a9375f3df63f0038e11b563874d129687500b364
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-05-31 17:11:52 +0200

    message read bug fix (fetching twice)

--- src/client/src/app/components/chat/chat.js
+++ src/client/src/app/components/chat/chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ChatInput from './chatInput'
 import ChatMessage from './chatMessage'
 import Api from "../../services";
@@ -6,98 +6,92 @@ import Api from "../../services";
 const URL = 'ws://localhost:3030';
 
 class Chat extends Component {
-  state = {
-    name: 'Bob',
-    messages: [],
-    dbMessages: [],
-  };
+    state = {
+        name: 'Bob',
+        messages: [],
+        dbMessages: [],
+    };
 
-  // ws = new WebSocket(URL)
+    // ws = new WebSocket(URL)
 
-  componentWillMount() {
-    Api.loadMessages()
-        .then((data) => {
-          this.setState({
-            dbMessages: data
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-  }
+    componentDidMount() {
+        Api.loadMessages()
+            .then((data) => {
+                console.log('RUN')
+                this.setState({
+                    dbMessages: data
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  // componentDidMount() {
+    // componentDidMount() {
     // this.ws.onopen = () => {
     //   // on connecting, do nothing but log it to the console
     //   console.log('connected')
     //   // this.setState(mess)
     // }
 
-  //   this.ws.onmessage = evt => {
-  //     // on receiving a message, add it to the list of messages
-  //     const message = JSON.parse(evt.data)
-  //     this.addMessage(message)
-  //   }
-  //
-  //   this.ws.onclose = () => {
-  //     console.log('disconnected')
-  //     // automatically try to reconnect on connection loss
-  //     this.setState({
-  //       ws: new WebSocket(URL),
-  //     })
-  //   }
-  // }
-  //
-  // addMessage = message => {
-  //   this.setState(state => {
-  //     const messages = state.messages.concat(message);
-  //     localStorage.setItem('conversation_id', messages)
-  //     return {
-  //       messages,
-  //     };
-  //   });
-  // }
+    //   this.ws.onmessage = evt => {
+    //     // on receiving a message, add it to the list of messages
+    //     const message = JSON.parse(evt.data)
+    //     this.addMessage(message)
+    //   }
+    //
+    //   this.ws.onclose = () => {
+    //     console.log('disconnected')
+    //     // automatically try to reconnect on connection loss
+    //     this.setState({
+    //       ws: new WebSocket(URL),
+    //     })
+    //   }
+    // }
+    //
+    // addMessage = message => {
+    //   this.setState(state => {
+    //     const messages = state.messages.concat(message);
+    //     localStorage.setItem('conversation_id', messages)
+    //     return {
+    //       messages,
+    //     };
+    //   });
+    // }
 
-  // submitMessage = messageString => {
-  //   // on submitting the ChatInput form, send the message, add it to the list and reset the input
-  //   const message = { name: this.state.name, message: messageString }
-  //   this.ws.send(JSON.stringify(message))
-  //   this.addMessage(message)
-  // }
+    // submitMessage = messageString => {
+    //   // on submitting the ChatInput form, send the message, add it to the list and reset the input
+    //   const message = { name: this.state.name, message: messageString }
+    //   this.ws.send(JSON.stringify(message))
+    //   this.addMessage(message)
+    // }
 
-  render() {
-    return (
-      <div>
-          {this.state.dbMessages.map((message) =>
-              <div className="other-parent" key={message._id}>
-                  <div className="message-bubble other-guy" key={message._id}>
-                      {message.content}
-                  </div>
-              </div>
-          )}
-        {/*<label htmlFor="name">*/}
-        {/*  Name:&nbsp;*/}
-        {/*  <input*/}
-        {/*    type="text"*/}
-        {/*    id={'name'}*/}
-        {/*    placeholder={'Enter your name...'}*/}
-        {/*    value={this.state.name}*/}
-        {/*    onChange={e => this.setState({ name: e.target.value })}*/}
-        {/*  />*/}
-        {/*</label>*/}
-        <ChatInput
-          ws={this.ws}
-          onSubmitMessage={messageString => this.submitMessage(messageString)}
-        />
-        {this.state.dbMessages.map((message, index) =>
-          <ChatMessage
-            key={index}
-            message={message.content}
-          />,
-        )}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {/*<label htmlFor="name">*/}
+                {/*  Name:&nbsp;*/}
+                {/*  <input*/}
+                {/*    type="text"*/}
+                {/*    id={'name'}*/}
+                {/*    placeholder={'Enter your name...'}*/}
+                {/*    value={this.state.name}*/}
+                {/*    onChange={e => this.setState({ name: e.target.value })}*/}
+                {/*  />*/}
+                {/*</label>*/}
+                <ChatInput
+                    ws={this.ws}
+                    onSubmitMessage={messageString => this.submitMessage(messageString)}
+                />
+                {this.state.dbMessages.map((message, index) =>
+                    <ChatMessage
+                        key={index}
+                        message={message.content}
+                    />,
+                )}
+            </div>
+        )
+    }
 }
 
 export default Chat
