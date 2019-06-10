1f864efa4ede894b407febb568da6a927cb04473
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-06-10 13:18:07 +0200

    dd

--- src/client/src/app/services/Api.js
+++ src/client/src/app/services/Api.js
@@ -1,4 +1,4 @@
-import openSocket from 'socket.io-client';
+// import openSocket from 'socket.io-client';
 // const  socket = openSocket('http://localhost:8000');
 
 // function subscribeToTimer(cb) {
