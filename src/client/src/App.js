e0eb11437173e19a537840119d4c6db178a2e4ae
indy <indymeer@student.arteveldehs.be>
2019-06-12 16:22:10 +0200

    validation

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -53,7 +53,6 @@ class App extends Component {
   getUser() {
     axios.get('/api/v1/users').then(response => {
       console.log('Get user response: ')
-      console.log(response.data)
       if (response.data.user) {
         console.log('Get User: There is a user saved in the server session: ')
 
