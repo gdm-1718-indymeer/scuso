5d6dc8b874155b56166cb667cad6bc2e7f572a02
Ruben <maesfranckxruben@gmail.com>
2019-06-14 20:28:04 +0200

    Added newsfeed on nav

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -93,6 +93,7 @@ class App extends Component {
                         <li><a href="/login">login</a></li>
                         <li><a href="/profile">profile</a></li>
                         <li><a href="/messaging">messages</a></li>
+                        <li><a href="/newsfeed">newsfeed</a></li>
                     </ul>
                     <ul className="secondNav">
                         <li><a href="http://www.maesfranckxruben.be/landingpage/">about</a></li>
