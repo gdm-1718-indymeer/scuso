1811550f69b9f9b5cefaef27063d474e0ecb1da8
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-06-16 02:59:30 +0200

    Loadconversation fixed

--- src/client/src/app/components/chat/chatMessage.js
+++ src/client/src/app/components/chat/chatMessage.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 export default ({ message, from }) =>
-    <div className={(from === 'Jonas' ? 'you-parent' : 'other-parent')}>
+    <div className={(from == localStorage.getItem('userId') ? 'you-parent' : 'other-parent')}>
 
-        <div className={"message-bubble " + (from === 'Jonas' ? 'you' : 'other-guy')}>
-            <p><strong>{from}:&nbsp;</strong></p><br />
+        <div className={"message-bubble " + (from == localStorage.getItem('userId') ? 'you' : 'other-guy')}>
             <p>{message}</p>
         </div>
     </div>
