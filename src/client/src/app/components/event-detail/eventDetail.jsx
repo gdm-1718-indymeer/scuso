785727c9e618e9153c7289b9f101762612e2d18e
Ruben <maesfranckxruben@gmail.com>
2019-06-16 19:12:45 +0200

    Revert "commit SCSS styling"
    This reverts commit a85a653869c03369746d38fc9103e8a236605b39.

---
+++ src/client/src/app/components/event-detail/eventDetail.jsx
@@ -0,0 +1,50 @@
+/*
+Import external libraries
+*/
+import React, { Component } from 'react';
+import Parser from 'html-react-parser';
+import classNames from 'classnames';
+
+/*
+Styling
+*/
+import './eventDetail.scss'
+
+class EventDetail extends Component {
+    
+    render() {
+
+        return (
+            <React.Fragment>
+                <div className="postdetailpage">
+                    <div className="body">
+                        <div className="headerwithsearch">
+                            <div className="header">
+                            <h2 className="discover">Newsfeed</h2>
+                            <p>What's new?</p> 
+                            </div>
+                        </div>
+                        <div className="contentdetail">
+                            <div className="detailpost">
+                                <h2>{this.props.with.title}</h2>
+                                
+                                <p>{this.props.with.body}</p>
+                                <img className="imagedetail" src={this.props.with.imageurl} />
+                            </div>
+                            <div className="info">
+                                <p><strong>Slug: </strong>{this.props.with.slug}</p>
+                                <p className="author"><strong>Author: </strong>{this.props.with.author}</p>
+                                <p className="author"><strong>filepath: </strong>{this.props.with.image}</p>
+                                <p className="author"><strong>filename: </strong>{this.props.with.imageurl}</p>
+                                <p className="author"><strong>category: </strong>{this.props.with.label}</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default (EventDetail);
--- src/client/src/app/pages/home/HomePage.jsx
+++ src/client/src/app/pages/home/HomePage.jsx
@@ -10,7 +10,6 @@ import Skeleton from 'react-loading-skeleton';
 /*
 Import internal libraries
 */
-import EventDetail from '../../components/event-detail/eventDetail';
 import Api from '../../services';
 import PostsList from '../../components/posts-list';
 import axios from 'axios';
@@ -76,13 +75,7 @@ class HomePage extends Component {
         }
     
     }
-    openNewsPost = async (event, title) => {
-        console.log('hallo')
-        console.log(title)
-        this.setState({
-            uid: title
-        })
-    }
+    
     deleteGhost() {
         this.setState({ ghost: [] })
     }
@@ -145,9 +138,7 @@ class HomePage extends Component {
 
     render() {
         const { posts } = this.state.posts;
-        if(this.state.uid){
-            return <EventDetail with={this.state.uid} />
-        }else{
+        
         return (
             <React.Fragment>
             
@@ -173,7 +164,7 @@ class HomePage extends Component {
                     <div className="container">
                     <div className="section__content section__content--articles">
 
-                    {this.state.ghost.map((item, index) =>
+                    {this.state.ghost.map((index) =>
                         <section className="card" key={index}>
                             <Skeleton height={150} />
                             <div className="card-detail">
@@ -184,7 +175,7 @@ class HomePage extends Component {
                         )}
                        
                        { this.state.posts.map((item, index) =>
-                        <section className="card" onClick={(ev) => this.openNewsPost(ev, item)} key={index}>
+                        <section className="card" key={index}>
                             <img className="card-image loading" src={item.imageurl}/>
                             <div className="card-detail">
                             <h3 className="card-title loading">{item.title}</h3>
@@ -266,7 +257,7 @@ class HomePage extends Component {
           pauseOnHover
           />
             </React.Fragment>
-        )}
+        )
     }
     
 }
